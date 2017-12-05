5c4fb5d7aa753d0a8bcce28d86aed86ac016e939
Maxim <karamba92@mail.ru>
2017-12-05 16:31:07 +0600

    #4 Админ может создавать Организаторов, добавлены два Организатора в сиды

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+Organizer.create!(name: 'GoogleOrg', description: 'Hi, I am from Google!')
+Organizer.create!(name: 'DockerOrg', description: 'Hi, I want teach you Docker container!')
