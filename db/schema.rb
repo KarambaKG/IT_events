adcf45d3fb395491256162128a78b69f8d71e20a
Maxim <karamba92@mail.ru>
2017-12-06 15:56:25 +0600

    #10 Перешёл на БД PostreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20171206071325) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.string "author_type"
-    t.integer "author_id"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
@@ -50,8 +53,8 @@ ActiveRecord::Schema.define(version: 20171206071325) do
     t.string "location"
     t.string "date_and_time"
     t.string "external_link"
-    t.integer "organizer_id"
-    t.integer "region_id"
+    t.bigint "organizer_id"
+    t.bigint "region_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar_file_name"
@@ -75,4 +78,6 @@ ActiveRecord::Schema.define(version: 20171206071325) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "organizers"
+  add_foreign_key "events", "regions"
 end
