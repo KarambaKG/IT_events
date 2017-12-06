2bf24e8104015eae1438a7c58feb83be022261cf
Maxim <karamba92@mail.ru>
2017-12-06 12:08:14 +0600

    #7 В админке можно создавать мероприятия, привязывать к организаторам и регионам. Добавил сиды мероприятий

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,11 @@ Organizer.create!(name: 'DockerOrg', description: 'Hi, I want teach you Docker c
 
 Region.create!(name: 'Амурская область')
 Region.create!(name: 'Алтайский край')
+
+Event.create!(name: 'Google in Amur!' , description: 'Лучшее событие Амура! Google и его возможности!',
+              city: 'Белогорск', location: 'Ул. Спортивная 10', date_and_time: '14 мая в 14.00',
+              image: 'подключи paperclip!', external_link: 'blive.kg', organizer_id: 1, region_id: 1)
+
+Event.create!(name: 'Docker in Altai!' , description: 'Лучшее событие Алтая! Docker и его возможности!',
+              city: 'Барнаул', location: 'Ул. Коммунистическая 22', date_and_time: '5 апреля в 12.00',
+              image: 'подключи paperclip!', external_link: 'namba.kg', organizer_id: 2, region_id: 2)
